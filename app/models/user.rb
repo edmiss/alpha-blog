4ccacb2018a8a1a30a8ea8601d158b3155268733
Hanzhao Chen <luoboshen@gmail.com>
2019-09-24 00:41:38 -0700

    add user validation;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
+  validates :username, presence: true, uniqueness: true,
+   length: { mininum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: true,
+   length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
 end
