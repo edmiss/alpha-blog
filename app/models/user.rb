29ffa9d508c62873ebc9263a9f79d4c0c57033cd
Hanzhao Chen <luoboshen@gmail.com>
2019-09-24 20:51:52 -0700

    create users association and add binding pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
+  has_many :articles 
+
+  before_save {
+    self.email = email.downcase
+}
+
   validates :username, presence: true, uniqueness: true,
    length: { mininum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: true,
