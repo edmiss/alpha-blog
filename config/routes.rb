16d20234688e629dd6c5d85cef328f209471cfea
Hanzhao Chen <luoboshen@gmail.com>
2019-09-24 23:38:05 -0700

    being able to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get "about", to: "pages#about"
   resources :articles 
+  resources :users, except:[:new]
+  get 'signup', to: 'users#new'
+
+
 end
